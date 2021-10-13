53b69de32a45fa0add04c12e490ec0c9fae24fe2
Fredesa <sebastianrodriguezvelasquez@gmail.com>
2021-10-13 11:13:44 -0500

    1

--- componentes/lib/src/pages/listview_file.dart
+++ componentes/lib/src/pages/listview_file.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/material.dart';
+
 class ListaPage extends StatefulWidget {
-  
   @override
   _ListaPageState createState() => _ListaPageState();
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> listaNumeros = [1,2,3,4,5];
+  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  int _ultimoItem = 0;
+  ScrollController _scrollController = new ScrollController();
+
+  void initState() {
+    super.initState();
+    _agregarImagenes();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregarImagenes();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Listas'),    
+        title: Text('Listas'),
       ),
       body: _crearlista(),
-    )
+    );
   }
 
   Widget _crearlista() {
+    return ListView.builder(
+        controller: _scrollController,
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int index) {
+          int imagen = _listaNumeros[index];
+          return FadeInImage(
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              image:
+                  NetworkImage('http://picsum.photos/500/300?image=$imagen'));
+        });
+  }
 
+  void _agregarImagenes() {
+    for (int i = 0; i <= 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+      setState(() {});
+    }
   }
 }
