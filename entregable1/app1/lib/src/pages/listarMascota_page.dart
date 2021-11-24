a14738661287bd4f8e3476e7095dd4fae8c2d6cf
Fredesa <sebastianrodriguezvelasquez@gmail.com>
2021-11-24 15:44:02 -0500

    Update listarMascota_page.dart

---
+++ entregable1/app1/lib/src/pages/listarMascota_page.dart
@@ -0,0 +1,149 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class enciclopediaPage extends StatelessWidget {
+  final db = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    User? user = _auth.currentUser;
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.of(context).pop(),
+            );
+          },
+        ),
+        title: Text("Tus Mascotas"),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: db
+            .collection('usuarios')
+            .doc(user!.uid)
+            .collection('mascotas')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else
+            return ListView(
+              children: snapshot.data!.docs.map((doc) {
+                return Card(
+                  child: ListTile(
+                    title: Text(doc['nombreMascota']),
+                    trailing: Icon(Icons.keyboard_arrow_right,
+                        color: Colors.deepPurple),
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => detailPage(post: doc))),
+                  ),
+                );
+              }).toList(),
+            );
+        },
+      ),
+    );
+  }
+}
+
+class detailPage extends StatefulWidget {
+  final DocumentSnapshot post;
+  detailPage({required this.post});
+
+  @override
+  _detailPageState createState() => _detailPageState();
+}
+
+class _detailPageState extends State<detailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.post.get("nombreMascota")),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Card(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 35),
+                Center(
+                  child: Text("Esta es la informacion de tu mascota",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      )),
+                ),
+                SizedBox(height: 100),
+                Center(
+                  child: Text("Edad: ${widget.post.get("laboratorio")}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ),
+                SizedBox(height: 35),
+                Center(
+                  child: Text("Raza: ${widget.post.get("formafarmaceutica")}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ),
+                SizedBox(height: 35),
+                Center(
+                  child: Text(
+                      "Tipo de Caracter: ${widget.post.get("viaadministracion")}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ),
+                SizedBox(height: 35),
+                Center(
+                  child: Text("Afeccion: ${widget.post.get("unidadmedida")}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ),
+                SizedBox(height: 35),
+                Center(
+                  child: Text("cantidad: ${widget.post.get("cantidad")}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
